9f3769640ea5c75099527fb5c5184e29bdb9ddd5
Kiyu <qconnection@gmail.com>
2014-08-06 16:36:28 +0200

    First commit

--- testing.py
+++ testing.py
@@ -6,10 +6,16 @@ class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		player_numbers = range(1,9)
 		self.render("template.html", title='title', player_numbers=player_numbers)
-			
+
+class SurpriseHandler(tornado.web.RequestHandler):
+	def get(self):
+		pic = 'http://i.imgur.com/THnYk7c.jpg'
+		pic_href = "<img src='" + pic + "'>"
+		self.write(pic_href)			
 
 
-application = tornado.web.Application([(r"/", MainHandler),])
+application = tornado.web.Application([(r"/", MainHandler),
+										(r"/surprise", SurpriseHandler)])
 
 if __name__ == "__main__":
 	application.listen(8888)
