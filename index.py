b6a727f894e3a7362a5f30a41aa75a13cae6ff8b
Kiyu <qconnection@gmail.com>
2014-08-12 18:56:16 +0200

    refactored some, but home.js breaks too easily, need to refactor further

--- index.py
+++ index.py
@@ -86,13 +86,6 @@ class MainHandler(tornado.web.RequestHandler):
 		self.render("home.html", title='title',  movie_info=movie_info )
 
 
-class SurpriseHandler(tornado.web.RequestHandler):
-	def get(self):
-		pic = 'http://i.imgur.com/THnYk7c.jpg'
-		pic_href = "<img src='" + pic + "'>"
-		self.write(pic_href)
-
-
 class ActorHandler(tornado.web.RequestHandler):
 	def post(self):
 		global MOVIE_LIST
@@ -101,43 +94,11 @@ class ActorHandler(tornado.web.RequestHandler):
 		imd.update(actor_object)
 		MOVIE_LIST = get_released_movies(actor_object)
 		print "Finished movie list"
-		# ACTOR = actor_object
-		# print actor_object['name']
 
-		# self.write(actor_object['mini biography'][0])
-
-
-
-class PlayerHandler(tornado.web.RequestHandler):
-	def post(self):
-		# global PLAYERS
-		# player_dict = self.get_argument('player_names')
-		# for i in range(len(player_dict)):
-		# 	player_index = 'Player' + str(i + 1 )
-		# 	PLAYERS[player_index] = player_dict[player_index]
-		global PLAYER
-		PLAYER = self.get_argument('player_name')
-		print PLAYER
-
-
-class GameHandler(tornado.web.RequestHandler):
-	def get(self):
-		player_name = self.get_argument('player1')
-		self.render("game_round.html", player=player_name)
-
-
-
-class MovieHandler(tornado.web.RequestHandler):
-	pass
-	
 
 application = tornado.web.Application([
 										(r"/", MainHandler),
-										(r"/surprise", SurpriseHandler),
 										(r"/getactor", ActorHandler),
-										(r"/game", GameHandler),
-										(r"/getplayer", PlayerHandler),
-										(r"/movie", MovieHandler)
 										],
 										static_path="static")
 
@@ -145,4 +106,5 @@ if __name__ == "__main__":
 	application.listen(8888)
 	tornado.autoreload.start()
 	tornado.autoreload.watch('home.html')
+	# tornado.autoreload.watch('static/home.js')
 	tornado.ioloop.IOLoop.instance().start()
