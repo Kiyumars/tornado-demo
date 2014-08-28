8f42d80087b6e633fd9a14690e4fea92b57b0b57
Kiyu <qconnection@gmail.com>
2014-08-28 14:20:42 +0200

    Fixed the movie discrimination functions, now have to deal with players and rendering. Not to mention the actual dbase actions.

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
+import sys
 import logging
 import random
 import json
 import urllib2
-import sys
 from bson.objectid import ObjectId
 
 import tornado.ioloop
@@ -42,6 +42,7 @@ class TestHandler(tornado.web.RequestHandler):
 			players[player.strip()] = 0
 		print players
 
+
 		game_id = db.game_input.insert({"Players": players, "Actor": actor_name})
 		print game_id
 		self.render("game_test.html", actor_name=actor_name, players=players, game_id=game_id)
@@ -79,7 +80,19 @@ class Game():
 
 class GameHandler(tornado.web.RequestHandler):
 	def get(self):
+		print "We are in GameHandler."
 		actor_name = self.get_argument('actor_entered')
+		print actor_name
+		actor_object = get_actor_object_from_imdb(actor_name)
+		print actor_object
+		# actor_db_entry_id = enter_actor_in_caching_db(actor_object)
+		# print actor_db_entry_id
+		movie_available = return_appropriate_movie(actor_object)
+		print movie_available
+		if not movie_available:
+			print "No more movies available from that actor. Exiting the game. Please play again soon, but choose a more prolific actor."
+			sys.exit()
+		movie, critics_score, audience_score = movie_available
 		# actor_name = "Nicolas Cage"
 		players_entry = self.get_argument('players')
 		players_list = players_entry.split(',')
@@ -88,31 +101,70 @@ class GameHandler(tornado.web.RequestHandler):
 			players[player.strip()] = 0
 
 		print players
-	
-		# game = Game(players)
-		# actor_id = actor_in_db(actor_name)
-		# print game.players
+		
 
-		actorObject, game.movie_list = search_info_on_imdb(actor_name)
-		print len(game.movie_list)
-		movie = pop_movie(game)
-		print len(game.movie_list)
-		update_movie_info(movie)
 
 
 		self.render("game_round.html", title='title', 
-					movie=movie, critics_score=77, audience_score=42)
+					movie=movie, critics_score=critics_score, audience_score=audience_score)
 	# # critics_score, audience_score = self.get_one_movie()
 	# # 		self.render("game_round.html", title='title',  movie=movie,
 	# # 				 critics_score=critics_score, audience_score=audience_score)
 	# # 		enter_actor_in_db(actorObject)
 
 	def post(self):
-		print len(game.movie_list)
-		movie = pop_movie(game)
-		print len(game.movie_list)
-		self.render("game_round.html", title='title', movie=movie, 
-					critics_score=None, audience_score=None)
+		pass
+
+def get_actor_object_from_imdb(actor_name):
+	actor_object = imd.search_person(actor_name)[0]
+	imd.update(actor_object)
+	
+	return actor_object
+
+
+def enter_actor_in_caching_db(actor_object):
+	pass
+
+
+def male_or_female(actor):
+	print "We are in male_or_female"
+	if actor['actor']:
+		return actor['actor']
+	else:
+		return actor['actress']
+
+
+def pick_random_movie_object(movie_list):
+	return movie_list.pop(random.randrange(len(movie_list) - 1 ) )
+
+
+def return_appropriate_movie(actor_object):
+	print "We are in return_appropriate_movie"
+	movie_list = male_or_female(actor_object)
+	print movie_list
+
+	if len(movie_list) < 1:
+		return False
+
+	useful = False
+	while not useful:
+		print "One round in the while loop of return_appropriate_movie"
+		movie = pick_random_movie_object(movie_list)
+		print movie['title']
+		if not movie_has_relevant_keys(movie):
+			continue
+		print "We moved beyond movie_has_relevant_keys"
+		has_reviews = check_for_rt_reviews(movie.movieID)
+		print has_reviews
+		if not has_reviews:
+			continue
+		critics_score, audience_score, rt_id = has_reviews
+		if not has_more_than_five_reviews(rt_id):
+			continue
+		useful = True
+	print "We are returning a movie. Hopefully"
+	print movie
+	return movie, critics_score, audience_score
 
 
 def pop_movie(GameObject):
@@ -132,6 +184,94 @@ def before_1990(movie):
 		return True
 	else:
 		return False
+
+def movie_has_relevant_keys(movie):
+	print "We are in movie_has_relevant_keys"
+	if "year" not in movie.keys():
+		return False
+	update_movie_info(movie)
+	print movie.keys()
+	if not all(key in movie.keys() for key in ("director", "plot outline", "plot",
+												 "cast", "full-size cover url")):
+		print "Not all keys"
+		return False
+	else:
+		print "Has all keys()"
+		return True
+
+
+def check_for_rt_reviews(movieID):
+	"""remove movies that have no reviews"""
+	print "We are in check_for_rt_reviews"
+	result = request_rt_ratings(movieID)
+	print result
+	if not result:
+		return False
+	critics, audience, rt_movie_id = result
+	if critics < 0 or not critics or not audience:
+		return False
+	elif not has_more_than_five_reviews(rt_movie_id):
+		return False
+	else:
+		return critics, audience, rt_movie_id
+
+def request_json_from_rt(movieID, request_type):
+	"""send a json request to RT.com for the movie's ratings scores"""
+	print "What is the request type? in request_json_from_rt"
+	print request_type
+
+	key= '5wzpmvz79dzzj8g5exucf8qv'
+	if request_type == "movie_alias":
+		print "movie_alias"
+		url_string = "http://api.rottentomatoes.com/api/public/v1.0/" + request_type + ".json?apikey=" + key + "&type=imdb&id=" + movieID
+		print url_string
+	elif request_type == "reviews":
+		print "reviews"
+		url_string = "http://api.rottentomatoes.com/api/public/v1.0/movies/" + movieID +"/reviews.json?apikey=" + key
+		print url_string
+	else:
+		print "something went wrong with request_json_from_rt"
+		sys.exit()
+
+	return json.load(urllib2.urlopen(url_string))
+
+
+def request_rt_ratings(movieID):
+	"""send a json request to RT.com for the movie's ratings scores"""
+	json_data = request_json_from_rt(movieID, "movie_alias")
+	
+	return parse_json_for_scores(json_data)
+
+def parse_json_for_scores(json_data):
+	"""parse json from RT.com's response if it has ratings scores"""
+	if 'ratings' in json_data:
+		critics_score = json_data['ratings']['critics_score']
+		audience_score = json_data['ratings']['audience_score']
+		rt_movie_id = json_data['id']
+		print "Print RT movie id"
+		print rt_movie_id
+		return critics_score, audience_score, rt_movie_id
+	else:
+		return False
+
+
+def has_more_than_five_reviews(movie_id):
+	total_reviews = request_number_of_rt_reviews(movie_id)
+	if total_reviews < 5:
+		return False
+	else:
+		return True
+
+def request_number_of_rt_reviews(imdb_id):
+	json_data = request_json_from_rt(str(imdb_id), "reviews")
+	print "Printing JSON Data"
+	print json_data
+
+	print parse_json_for_total_reviews_number(json_data)
+	return parse_json_for_total_reviews_number(json_data)
+
+def parse_json_for_total_reviews_number(json_data):
+	return int(json_data['total'])
 
 
 def get_released_movies(actor):
@@ -254,38 +394,12 @@ class Movie():
 			self.audience_score = entry['Audience Rating']
 
 
-def reviews_exist(movieID):
-	"""remove movies that have no reviews or just one positive review on RT.com"""
-	critics, audience = request_rt_ratings(movieID)
-	if critics < 0 or critics == 100 or not critics:
-		return False, False, False
-	else:
-		return True, critics, audience
+
 
 
 def update_movie_info(movie):
-	imd.update(movie)
-
-
-def request_rt_ratings(movieID):
-	"""send a json request to RT.com for the movie's ratings scores"""
-	key= '5wzpmvz79dzzj8g5exucf8qv'
-	url_string = "http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?apikey=" + key + "&type=imdb&id=" + movieID
-	
-	json_data = json.load(urllib2.urlopen(url_string))
-	critics, audience = parse_json(json_data)
-
-	return critics, audience
-
-
-def parse_json(json_data):
-	"""parse json from RT.com's response if it has ratings scores"""
-	if 'ratings' in json_data:
-		critics_score = json_data['ratings']['critics_score']
-		audience_score = json_data['ratings']['audience_score']
-		return critics_score, audience_score
-	else:
-		return False, False
+	print "updating movie"
+	return imd.update(movie)
 
 
 def enter_filmography(actor_id, filmography, critics_score, audience_score):
