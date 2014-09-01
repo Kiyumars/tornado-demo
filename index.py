0b3a5dcf8189b1e9a9981766788e8ae8e96c1312
Kiyu <qconnection@gmail.com>
2014-09-01 15:39:12 +0200

    Now one movie can be entered into the actors db document, and there are game sessions. Need to work on putting all the info in the game sessions db

--- index.py
+++ index.py
@@ -25,6 +25,42 @@ class MainHandler(tornado.web.RequestHandler):
 		self.render("home.html", title='title',  movie=None, critics_score=None, audience_score=None )
 
 
+class GameHandler(tornado.web.RequestHandler):
+	def get(self):
+		players_entry = self.get_argument('players')
+		players_list = players_entry.split(',')
+		players = {}
+		# players = {"Philip": 0, "Michael": 0}
+		for player in players_list:
+			print player
+			players[player.strip()] = 0
+
+		game_id = start_game_session(players)
+		print "We are in GameHandler."
+		actor_name = self.get_argument('actor_entered')
+		print actor_name
+		Actor = get_actor_object_from_imdb(actor_name)
+		print Actor
+		enter_actor_in_actors_db(Actor)
+		# actor_db_entry_id = enter_actor_in_caching_db(actor_object)
+		# print actor_db_entry_id
+		movie_available = return_appropriate_movie(Actor)
+		if not movie_available:
+			print "No more movies available from that actor. Exiting the game. Please play again soon, but choose a more prolific actor."
+		movie, critics_score, audience_score = movie_available
+		enter_movie_in_actors_db(movie, Actor.personID, critics_score, audience_score)
+		enter_ratings_scores_in_game_db(game_id, critics_score, audience_score)
+		#enter all movies in both databases in an asynchronous loop
+		
+
+		self.render("game_round.html", title='title', 
+					movie=movie, critics_score=critics_score, audience_score=audience_score,
+					players=players, game_id=game_id)
+
+
+	def post(self):
+		pass
+
 # class TestHandler(tornado.web.RequestHandler):
 # 	def get(self):
 # 		print "We are in TestHandler"
@@ -65,52 +101,6 @@ class MainHandler(tornado.web.RequestHandler):
 # 		print db.game_input.update({"_id": ObjectId(game_id)}, {"Players": players})
 # 		self.render("score_update.html", players=players, game_id=game_id)
 
-class Game():
-	def __init__(self):
-		self.players = None
-		self.movie_list = None
-
-	def pop_movie_from_list(self):
-		return self.movie_list.pop(random.randrange(len(self.movie_list)))
-
-	def update_player_scores(self, score_update):
-		for player in self.players:
-			self.players[player] += score_update[player] 
-
-
-class GameHandler(tornado.web.RequestHandler):
-	def get(self):
-		print "We are in GameHandler."
-		actor_name = self.get_argument('actor_entered')
-		print actor_name
-		actor_object = get_actor_object_from_imdb(actor_name)
-		print actor_object
-		# actor_db_entry_id = enter_actor_in_caching_db(actor_object)
-		# print actor_db_entry_id
-		movie_available = return_appropriate_movie(actor_object)
-		print movie_available
-		if not movie_available:
-			print "No more movies available from that actor. Exiting the game. Please play again soon, but choose a more prolific actor."
-			sys.exit()
-		movie, critics_score, audience_score = movie_available
-		# actor_name = "Nicolas Cage"
-		players_entry = self.get_argument('players')
-		players_list = players_entry.split(',')
-		players = {}
-		# players = {"Philip": 0, "Michael": 0}
-		for player in players_list:
-			print player
-			players[player.strip()] = 0
-
-		print players
-		
-		self.render("game_round.html", title='title', 
-					movie=movie, critics_score=critics_score, audience_score=audience_score,
-					players=players)
-
-
-	def post(self):
-		pass
 
 def get_actor_object_from_imdb(actor_name):
 	actor_object = imd.search_person(actor_name)[0]
@@ -119,11 +109,64 @@ def get_actor_object_from_imdb(actor_name):
 	return actor_object
 
 
-def enter_actor_in_caching_db(actor_object):
-	pass
+def start_game_session(players):
+	player_guesses = {}
+	for player in players:
+		player_guesses[player] = 0
+	game_id = db.game_sessions.insert({
+										"Player scores": players,
+										"Player guesses": player_guesses 
+										})
+
+	return game_id
 
 
-def male_or_female(actor):
+def enter_ratings_scores_in_game_db(game_id, critics_score, audience_score):
+	db.game_sessions.update({"_id": game_id}, {"Critics": critics_score,
+												"Audience": audience_score
+												}, True)
+
+
+def actor_in_db(actor_name):
+	actor_PersonID = False
+	split_name = actor_name.split(' ')
+	print split_name
+	#remove count from number_actors_in_db
+	number_actors_in_db = db.actors.find({ "$and": [{'Last Name': split_name [1].strip()},
+													{'First Name': split_name[0].strip()} ] }).count() 
+	print number_actors_in_db	
+ 	
+ 	if number_actors_in_db > 0:
+ 		return True
+ 		# actor_search = db.actors.find({'Last Name': str(split_name[1]), 
+			# 								'First Name': str(split_name[0])} , 
+			# 								{'PersonID': 1, '_id': 0})
+ 		# for actor in actor_search:
+			# actor_personID = actor['PersonID']
+ 		# return True, actor_personID
+ 	else:
+ 		# return False, False
+ 		return False
+
+
+
+def enter_game_session_info_in_game_db(critics_score, audience_score, players):
+	player_guesses = {}
+	for player in players:
+		player_guesses[player] = 0
+
+	game_id = db.game_sessions.insert({"Player scores": players, 
+										"Critics": critics_score, 
+										"Audience": audience_score, 
+										"Player guesses": player_guesses,
+										"Movies": []
+										})
+
+	return game_id
+
+
+
+def actor_or_actress(actor):
 	print "We are in male_or_female"
 	if 'actor' in actor.keys():
 		return actor['actor']
@@ -137,50 +180,28 @@ def pick_random_movie_object(movie_list):
 
 def return_appropriate_movie(actor_object):
 	print "We are in return_appropriate_movie"
-	movie_list = male_or_female(actor_object)
-	print movie_list
-
-	if len(movie_list) < 1:
-		return False
+	movie_list = actor_or_actress(actor_object)
 
 	useful = False
-	while not useful:
-		print "One round in the while loop of return_appropriate_movie"
+	while not useful and len(movie_list) > 0:
 		movie = pick_random_movie_object(movie_list)
-		print movie['title']
+
 		if not movie_has_relevant_keys(movie):
 			continue
-		print "We moved beyond movie_has_relevant_keys"
 		has_reviews = check_for_rt_reviews(movie.movieID)
-		print has_reviews
 		if not has_reviews:
 			continue
 		critics_score, audience_score, rt_id = has_reviews
 		if not has_more_than_five_reviews(rt_id):
 			continue
 		useful = True
-	print "We are returning a movie. Hopefully"
+	
+	if not useful:
+		return False
+	print "We are returning a movie."
 	print movie
 	return movie, critics_score, audience_score
 
-
-def pop_movie(GameObject):
-	cond = False
-	while not cond:
-		movie = GameObject.pop_movie_from_list()
-		if before_1990(movie):
-			print "Popped a movie before 1990"
-			return movie
-			cond = True
-		else:
-			print "Did not find movie before 1990"	
-
-
-def before_1990(movie):
-	if movie['year'] < 1990:
-		return True
-	else:
-		return False
 
 def movie_has_relevant_keys(movie):
 	print "We are in movie_has_relevant_keys"
@@ -228,7 +249,7 @@ def request_json_from_rt(movieID, request_type):
 		print url_string
 	else:
 		print "something went wrong with request_json_from_rt"
-		sys.exit()
+
 
 	return json.load(urllib2.urlopen(url_string))
 
@@ -239,14 +260,14 @@ def request_rt_ratings(movieID):
 	
 	return parse_json_for_scores(json_data)
 
+
 def parse_json_for_scores(json_data):
 	"""parse json from RT.com's response if it has ratings scores"""
 	if 'ratings' in json_data:
 		critics_score = json_data['ratings']['critics_score']
 		audience_score = json_data['ratings']['audience_score']
 		rt_movie_id = json_data['id']
-		print "Print RT movie id"
-		print rt_movie_id
+
 		return critics_score, audience_score, rt_movie_id
 	else:
 		return False
@@ -254,7 +275,7 @@ def parse_json_for_scores(json_data):
 
 def has_more_than_five_reviews(movie_id):
 	total_reviews = request_number_of_rt_reviews(movie_id)
-	if total_reviews < 5:
+	if total_reviews < 6:
 		return False
 	else:
 		return True
@@ -268,72 +289,63 @@ def request_number_of_rt_reviews(imdb_id):
 	return parse_json_for_total_reviews_number(json_data)
 
 def parse_json_for_total_reviews_number(json_data):
-	return int(json_data['total'])
+	return json_data['total']
 
 
-def get_released_movies(actor):
-	"""only include movies before current year, to exclude unreleased films"""
-	movie_list = []
+def enter_actor_in_actors_db(actor):
 	
-	try:
-		for movie in actor['actor']:			
-			if 'year' not in movie.keys() or movie['year'] >= 2014:
-				continue
-			else:
-				movie_list.append(movie)
-			#some movies don't have year values. Perhaps find another way to handle error
-	except KeyError:
-		for movie in actor['actress']:
-			if 'year' not in movie.keys() or movie['year'] >= 2014:
-					continue
-			else:
-				movie_list.append(movie)
-
-	return movie_list
-
-
-def search_info_on_imdb(actor_name):
-		actor_object = imd.search_person(actor_name)[0]
-		imd.update(actor_object)
-		return actor_object, get_released_movies(actor_object)
-		# movie, critics_score, audience_score = get_one_movie()
-		# return movie, critics_score, audience_score
-		# return actor_object
-
-
-def enter_actor_in_db(actor, movie_list):
-	print str(len(movie_List)) + " movies in total to enter into database."
 	split_name = actor['canonical name'].split(",")
-	total_movie_dict = []
-
-	for movie in movie_List:
-		update_movie_info(movie)
-
-		movie_dict = {'Title': movie['title'], 
-						'Year': movie['year'],
-			 			'Director': str(movie['director'][0]['name']),
-			 			'Plot Outline': movie['plot outline'],
-			 			'Plot': movie['plot'],
-			 			# 'Poster': movie['full-size cover url']  
-			 			}
-
-		cast = ''
-		for actor in movie['cast'][:5]:
-			cast += actor['name'] + ", "
-		movie_dict['Cast'] = cast
-		total_movie_dict.append(movie_dict)
-		print "Finished " + str(len(total_movie_dict)) + " of " + str(len(MOVIE_LIST)) + " movies."
 
 	db.actors.insert({
-						"IMDb PersonID": actor.personID, 
-						"Last Name": split_name[0].strip(),
-						"First Name": split_name[1].strip(),
-						# "Birth year": actor['birth date'],
-						# "Biography": actor['bio'],
-						"Movies": total_movie_dict
+					"IMDb PersonID": actor.personID, 
+					"Last Name": split_name[0].strip(),
+					"First Name": split_name[1].strip(),
+					# "Birth year": actor['birth date'],
+					# "Biography": actor['bio'],
+					"Movies": []
 
-						})
-	print "Finished saving all of the movies from this actor/actress."
+					})
+
+def enter_movie_in_actors_db(movie, actor_id, critics_score, audience_score):
+
+	movie_dict = prepare_movie_dict_entry(movie, critics_score, audience_score)
+
+	db.actors.update({"IMDb PersonID": actor_id}, { "$push": {"Movies": movie_dict}})
+
+	print "Entered one movie of this actor/actress."
+
+
+def enter_all_movies_in_both_dbases(movie_list, actor_id, game_id, 
+									critics_score, audience_score):
+	while len(movie_list) > 0:
+		movie = return_appropriate_movie(movie_list)
+		enter_movie_in_actor_db(movie, actor_id, critics_score, audience_score)
+		enter_movie_into_game_db(movie, game_id, critics_score, audience_score)
+
+
+def enter_movie_into_game_db(movie, game_id, 
+								critics_score, audience_score):
+	movie_dict = prepare_movie_dict_entry(movie, critics_score, audience_score)
+	db.game_sessions.update({"_id": game_id}, {"$push": {"Movies": movie_dict}})
+
+
+def prepare_movie_dict_entry(movie, critics_score, audience_score):
+	movie_dict = {'Title': movie['title'], 
+					'Year': movie['year'],
+		 			'Director': str(movie['director'][0]['name']),
+		 			'Plot Outline': movie['plot outline'],
+		 			'Plot': movie['plot'],
+		 			'Poster': movie['full-size cover url'],
+		 			"Critics": critics_score,
+		 			"Audience": audience_score  
+		 			}
+
+	cast = []
+	for i in range(len(movie['cast'])):
+		cast.append(movie['cast'][i]['name'])
+	movie_dict['Cast'] = cast
+
+	return movie_dict
 
 
 def search_movieIDs_on_mongodb(actor_id):
@@ -351,26 +363,6 @@ def search_movieIDs_on_mongodb(actor_id):
 	# 	movie_info.append(movie_entry)
 
 
-def actor_in_db(actor_name):
-	actor_PersonID = False
-	split_name = actor_name.split(' ')
-	print split_name
-	#remove count from number_actors_in_db
-	number_actors_in_db = db.actors.find({ "$and": [{'Last Name': split_name [1].strip()},
-													{'First Name': split_name[0].strip()} ] }).count() 
-	print number_actors_in_db	
- 	
- 	if number_actors_in_db > 0:
- 		return True
- 		# actor_search = db.actors.find({'Last Name': str(split_name[1]), 
-			# 								'First Name': str(split_name[0])} , 
-			# 								{'PersonID': 1, '_id': 0})
- 		# for actor in actor_search:
-			# actor_personID = actor['PersonID']
- 		# return True, actor_personID
- 	else:
- 		# return False, False
- 		return False
 
 
 
@@ -389,9 +381,6 @@ class Movie():
 			self.plot = entry['Plot']
 			self.critics_score = entry['Critics Rating']
 			self.audience_score = entry['Audience Rating']
-
-
-
 
 
 def update_movie_info(movie):
